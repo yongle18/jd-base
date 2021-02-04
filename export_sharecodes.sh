9b744486ef8f53d4f3a6a443d382ec87ccb35fab
yongle18 <2077858@qq.com>
2021-02-04 14:46:35 +0800

    Add files via upload

--- export_sharecodes.sh
+++ export_sharecodes.sh
@@ -1,98 +1,47 @@
 #!/usr/bin/env bash
 
-## Author: Evine Deng
-## Source: https://github.com/EvineDeng/jd-base
-## Modified： 2021-01-12
-## Version： v3.4.0
-
-## 文件路径、脚本网址、文件版本以及各种环境的判断
-if [ -f /proc/1/cgroup ]
-then
-  isDocker=$(cat /proc/1/cgroup | grep docker)
-else
-  isDocker=""
-fi
-
-if [ -z "${isDocker}" ]
-then
-  ShellDir=$(cd $(dirname $0); pwd)
-else
-  ShellDir=${JD_DIR}
-fi
-
+## 路径、环境判断
+ShellDir=${JD_DIR:-$(cd $(dirname $0); pwd)}
 LogDir=${ShellDir}/log
-ScriptsDir=${ShellDir}/scripts
-FileConf=${ShellDir}/config/config.sh
-DateToday=$(date "+%Y-%m-%d")
-isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-if [[ ${isTermux} ]]
-then
-  Opt="P"
-else
-  Opt="E"
-fi
+[[ ${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT} ]] && Opt="P" || Opt="E"
+Tips="从日志中未找到任何互助码..."
 
-## 导出互助码子程序
+## 所有有互助码的活动，只需要把脚本名称去掉前缀jd_后列在Name1中，将其中文名称列在Name2中即可。Name1和Name2中两个名称必须一一对应。
+Name1=(fruit pet plantBean dreamFactory jdfactory crazy_joy jdzz jxnc bookshop cash festival immortal nh sgmh newYearMoney)
+Name2=(东东农场 东东萌宠 京东种豆得豆 京喜工厂 东东工厂 crazyJoy任务 京东赚赚 京喜农场 口袋书店 签到领现金 手机年终奖 神仙书院 年货节 闪购盲盒 压岁钱)
+
+## 导出互助码的通用程序
 function Cat_Scodes {
-  cd ${LogDir}/jd_$1
-  for log in $(ls -r)
-  do
-    codes=$(grep -${Opt} $2 ${log} | perl -pe "s| ||")
-    [ -n "${codes}" ] && break
-  done
-  echo ${codes}
-}
-
-## 导出口袋书店互助码
-function Cat_ScodesBookShop {
-  cd ${LogDir}/jd_bookshop
-  for log in $(ls -r)
-  do
-    codes=$(perl -pe "s|信息获取成功\n||g" ${log} | grep -${Opt} "您的好友助力码为" | perl -pe "{s|您的好友助力码为||g; s|用户||g}")
-    [ -n "${codes}" ] && break
-  done
-  echo ${codes}
-}
-
-## 导出签到领现金互助码
-function Cat_ScodesCash {
-  cd ${LogDir}/jd_cash
-  for log in $(ls -r)
-  do
-    codes=$(perl -0777 -pe "s|\*+\n+||g" ${log} | grep -${Opt} "您的助力码为" | perl -pe "{s|\*+开始||g; s|您的助力码为|：|g}")
-    [ -n "${codes}" ] && break
-  done
-  echo ${codes}
+  if [ -d ${LogDir}/jd_$1 ] && [[ $(ls ${LogDir}/jd_$1) != "" ]]; then
+    cd ${LogDir}/jd_$1
+    for log in $(ls -r); do
+      case $# in
+        1)
+          codes=$(cat ${log} | grep -${Opt} "开始【京东账号|您的(好友)?助力码为" | uniq | perl -0777 -pe "{s|\*||g; s|开始||g; s|\n您的(好友)?助力码为(：)?:?|：|g; s|，.+||g}" | perl -ne '{print if /：/}')
+          ;;
+        2)
+          codes=$(grep -${Opt} $2 ${log} | perl -pe "{s| ||g; s|$2||g}")
+          ;;
+      esac
+      [[ ${codes} ]] && break
+    done
+    [[ ${codes} ]] && echo "${codes}" || echo ${Tips}
+  else
+    echo "还没有运行过 jd_$1 脚本，没有产生日志..."
+  fi
 }
 
 ## 汇总
 function Cat_All {
-  echo -e "本脚本从最后一个正常的日志中寻找互助码，某些账号缺失则代表在最后一个正常的日志中没有找到。"
-  echo -e "\n东东农场："
-  Cat_Scodes fruit "的东东农场好友互助码" | perl -pe "s|的东东农场好友互助码||g"
-  echo -e "\n东东萌宠："
-  Cat_Scodes pet "的东东萌宠好友互助码" | perl -pe "s|的东东萌宠好友互助码||g"
-  echo -e "\n种豆得豆："
-  Cat_Scodes plantBean "的京东种豆得豆好友互助码" | perl -pe "s|的京东种豆得豆好友互助码||g"
-  echo -e "\n京喜工厂："
-  Cat_Scodes dreamFactory "的京喜工厂好友互助码" | perl -pe "s|的京喜工厂好友互助码||g"
-  echo -e "\n东东工厂："
-  Cat_Scodes jdfactory "的东东工厂好友互助码" | perl -pe "s|的东东工厂好友互助码||g"
-  echo -e "\n疯狂的JOY："
-  Cat_Scodes crazy_joy "的crazyJoy任务好友互助码" | perl -pe "s|的crazyJoy任务好友互助码||g"
-  echo -e "\n京东赚赚："
-  Cat_Scodes jdzz "的京东赚赚好友互助码" | perl -pe "s|的京东赚赚好友互助码||g"
-  echo -e "\n京喜农场："
-  Cat_Scodes jxnc "的京喜农场好友互助码" | perl -pe "s|的京喜农场好友互助码||g"
-  echo -e "\n口袋书店："
-  Cat_ScodesBookShop
-  echo -e "\n签到领现金："
-  Cat_ScodesCash
+  echo -e "\n本脚本从最后一个正常的日志中寻找互助码，某些账号缺失则代表在最后一个正常的日志中没有找到。"
+  for ((i=0; i<${#Name1[*]}; i++)); do
+    echo -e "\n${Name2[i]}："
+    [[ $(Cat_Scodes "${Name1[i]}" "的${Name2[i]}好友互助码") == ${Tips} ]] && Cat_Scodes "${Name1[i]}" || Cat_Scodes "${Name1[i]}" "的${Name2[i]}好友互助码"
+  done
 }
 
 ## 执行并写入日志
 LogTime=$(date "+%Y-%m-%d-%H-%M-%S")
 LogFile="${LogDir}/export_sharecodes/${LogTime}.log"
 [ ! -d "${LogDir}/export_sharecodes" ] && mkdir -p ${LogDir}/export_sharecodes
-touch ${LogFile}
-Cat_All | perl -pe "s| |\n|g" | tee ${LogFile}
+Cat_All | perl -pe "{s|京东种豆|种豆|; s|crazyJoy任务|疯狂的JOY|}" | tee ${LogFile}
--- git_pull.sh
+++ git_pull.sh
@@ -1,36 +1,19 @@
 #!/usr/bin/env bash
 
-## Author: Evine Deng
-## Source: https://github.com/EvineDeng/jd-base
-## Modified： 2021-01-14
-## Version： v3.5.0
-
 ## 文件路径、脚本网址、文件版本以及各种环境的判断
-if [ -f /proc/1/cgroup ]
-then
-  isDocker=$(cat /proc/1/cgroup | grep docker)
-else
-  isDocker=""
-fi
-
-if [ -z "${isDocker}" ]
-then
-  ShellDir=$(cd $(dirname $0); pwd)
-  ShellJd=${ShellDir}/jd.sh
-else
-  ShellDir=${JD_DIR}
-  ShellJd=jd
-fi
-
+ShellDir=${JD_DIR:-$(cd $(dirname $0); pwd)}
+[[ ${JD_DIR} ]] && ShellJd=jd || ShellJd=${ShellDir}/jd.sh
 LogDir=${ShellDir}/log
 [ ! -d ${LogDir} ] && mkdir -p ${LogDir}
-
 ScriptsDir=${ShellDir}/scripts
+Scripts2Dir=${ShellDir}/scripts2
 ConfigDir=${ShellDir}/config
 FileConf=${ConfigDir}/config.sh
 FileDiy=${ConfigDir}/diy.sh
 FileConfSample=${ShellDir}/sample/config.sh.sample
 ListCron=${ConfigDir}/crontab.list
+ListCronLxk=${ScriptsDir}/docker/crontab_list.sh
+ListCronShylocks=${Scripts2Dir}/docker/crontab_list.sh
 ListTask=${LogDir}/task.list
 ListJs=${LogDir}/js.list
 ListJsAdd=${LogDir}/js-add.list
@@ -40,36 +23,40 @@ ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-WhichDep=$(grep "/jd-base" "${ShellDir}/.git/config")
+ScriptsURL=https://gitee.com/lxk0301/jd_scripts
 
-if [[ ${WhichDep} == *github* ]]; then
-  ScriptsURL=https://github.com/lxk0301/jd_scripts
-  ShellURL=https://github.com/EvineDeng/jd-base
-else
-  ScriptsURL=https://gitee.com/lxk0301/jd_scripts
-  ShellURL=https://gitee.com/evine/jd-base
-fi
-
-## 更新shell脚本
-function Git_PullShell {
-  echo -e "更新shell脚本，原地址：${ShellURL}\n"
-  cd ${ShellDir}
-  git fetch --all
-  ExitStatusShell=$?
-  git reset --hard origin/v3
+## 更新crontab，gitee服务器同一时间限制5个链接，因此每个人更新代码必须错开时间，每次执行git_pull随机生成。
+## 每天次数随机，更新时间随机，更新秒数随机，至少6次，至多12次，大部分为8-10次，符合正态分布。
+function Update_Cron {
+  if [ -f ${ListCron} ]; then
+    RanMin=$((${RANDOM} % 60))
+    RanSleep=$((${RANDOM} % 56))
+    RanHourArray[0]=$((${RANDOM} % 3))
+    for ((i=1; i<14; i++)); do
+      j=$(($i - 1))
+      tmp=$((${RANDOM} % 3 + ${RanHourArray[j]} + 2))
+      [[ ${tmp} -lt 24 ]] && RanHourArray[i]=${tmp} || break
+    done
+    RanHour=${RanHourArray[0]}
+    for ((i=1; i<${#RanHourArray[*]}; i++)); do
+      RanHour="${RanHour},${RanHourArray[i]}"
+    done
+    perl -i -pe "s|.+(bash git_pull.+)|${RanMin} ${RanHour} \* \* \* sleep ${RanSleep} && \1|" ${ListCron}
+    crontab ${ListCron}
+  fi
 }
 
-## 克隆js脚本
+## 克隆scripts
 function Git_CloneScripts {
-  echo -e "克隆JS脚本，原地址：${ScriptsURL}\n"
+  # echo -e "克隆LXK9301脚本，原地址：${ScriptsURL}\n"
   git clone -b master ${ScriptsURL} ${ScriptsDir}
   ExitStatusScripts=$?
   echo
 }
 
-## 更新js脚本
+## 更新scripts
 function Git_PullScripts {
-  echo -e "更新JS脚本，原地址：${ScriptsURL}\n"
+  # echo -e "更新LXK9301脚本，原地址：${ScriptsURL}\n"
   cd ${ScriptsDir}
   git fetch --all
   ExitStatusScripts=$?
@@ -80,45 +67,28 @@ function Git_PullScripts {
 ## 用户数量UserSum
 function Count_UserSum {
   i=1
-  while [ ${i} -le 1000 ]
-  do
-    TmpCK=Cookie${i}
-    eval CookieTmp=$(echo \$${TmpCK})
-    if [ -n "${CookieTmp}" ]
-    then
-      UserSum=${i}
-    else
-      break
-    fi
+  while [ $i -le 1000 ]; do
+    Tmp=Cookie$i
+    CookieTmp=${!Tmp}
+    [[ ${CookieTmp} ]] && UserSum=$i || break
     let i++
   done
 }
 
 ## 把config.sh中提供的所有账户的PIN附加在jd_joy_run.js中，让各账户相互进行宠汪汪赛跑助力
-## 你的账号将按Cookie顺序被优先助力，助力完成再助力我的账号和lxk0301大佬的账号
 function Change_JoyRunPins {
   j=${UserSum}
   PinALL=""
-  while [ ${j} -ge 1 ]
+  while [[ $j -ge 1 ]]
   do
-    TmpCK=Cookie${j}
-    eval CookieTemp=$(echo \$${TmpCK})
+    Tmp=Cookie$j
+    CookieTemp=${!Tmp}
     PinTemp=$(echo ${CookieTemp} | perl -pe "{s|.*pt_pin=(.+);|\1|; s|%|\\\x|g}")
     PinTempFormat=$(printf ${PinTemp})
     PinALL="${PinTempFormat},${PinALL}"
     let j--
   done
-  PinEvine="Evine,做一颗潇洒的蛋蛋,Evine007,jd_7bb2be8dbd65c,jd_6fae2af082798,jd_664ecc3b78945,277548856_m,米大眼老鼠,"
-  PinALL="${PinALL}${PinEvine}"
   perl -i -pe "{s|(let invite_pins = \[\")(.+\"\];?)|\1${PinALL}\2|; s|(let run_pins = \[\")(.+\"\];?)|\1${PinALL}\2|}" ${ScriptsDir}/jd_joy_run.js
-}
-
-## 将我的invitecode追加到脚本中，不会删除原作者的邀请码
-function Change_InviteCode {
-  CodeZz="  'Sy7sqHks\@Sa0zZlJapLMZw9pdDQnOoo2clfysC8H5a\@S5KkcRhgdoAeEI0jznP4OcQ\@SvPp0RRoR_VHRT0c\@S5KkcRkhIoFaGdhr8lvADfA',\n  'S5KkcRkpK8QLWdU7ykvMIdw\@SaG_llbW3LM1L9qFNQWOgo2Qw\@SaXzwlYqOIvhb-KpFTXua\@Sy7sqHksZ9VM',"
-  CodeJoy=",\n  'i7J-rBjC1cY=\@9Lz36oup9_3x1O3gdANrI0MGRhplILGlq33N3lhoF4Q=\@TZaj4q_GSarkd-u40-hYJg==\@aEYNdH9WkHKZzdje-aDvWqt9zd5YaBeE\@7ZiMxCUnP2Orfc3eWGgXhA==',\n  'ZKfuxUZxKdGbDxTmAHnqkqt9zd5YaBeE\@xWXlN8vLwpFOy71e_SEYsg==\@ym8TOcaoUTQnJZKpDzKWd6t9zd5YaBeE\@9_dxd9S1-R7nohQ1FGiupUGIzB-QNOGN'"
-  perl -0777 -i -pe "s|(const inviteCodes = \[\n)(.+\n.+\n\])|\1${CodeZz}\n\2|" ${ScriptsDir}/jd_jdzz.js >/dev/null 2>&1
-  perl -0777 -i -pe "s|(const inviteCodes = \[\n)(.+\n.+)(\n\];?)|\1\2${CodeJoy}\3|" ${ScriptsDir}/jd_crazy_joy.js >/dev/null 2>&1
 }
 
 ## 修改lxk0301大佬js文件的函数汇总
@@ -128,25 +98,25 @@ function Change_ALL {
     if [ -n "${Cookie1}" ]; then
       Count_UserSum
       Change_JoyRunPins
-      Change_InviteCode
     fi
   fi
 }
 
+## 检测文件：LXK9301/jd_scripts 仓库中的 docker/crontab_list.sh
 ## 检测定时任务是否有变化，此函数会在Log文件夹下生成四个文件，分别为：
 ## task.list    crontab.list中的所有任务清单，仅保留脚本名
-## js.list      scripts/docker/crontab_list.sh文件中用来运行js脚本的清单（去掉后缀.js，非运行脚本的不会包括在内）
-## js-add.list  如果 scripts/docker/crontab_list.sh 增加了定时任务，这个文件内容将不为空
-## js-drop.list 如果 scripts/docker/crontab_list.sh 删除了定时任务，这个文件内容将不为空
+## js.list      上述检测文件中用来运行js脚本的清单（去掉后缀.js，非运行脚本的不会包括在内）
+## js-add.list  如果上述检测文件增加了定时任务，这个文件内容将不为空
+## js-drop.list 如果上述检测文件删除了定时任务，这个文件内容将不为空
 function Diff_Cron {
   if [ -f ${ListCron} ]; then
-    if [ -n "${isDocker}" ]
+    if [ -n "${JD_DIR}" ]
     then
-      grep -E " j[drx]_\w+" ${ListCron} | perl -pe "s|.+ (j[drx]_\w+).*|\1|" | uniq | sort > ${ListTask}
+      grep -E " j[drx]_\w+" ${ListCron} | perl -pe "s|.+ (j[drx]_\w+).*|\1|" | sort -u > ${ListTask}
     else
-      grep "${ShellDir}/" ${ListCron} | grep -E " j[drx]_\w+" | perl -pe "s|.+ (j[drx]_\w+).*|\1|" | uniq | sort > ${ListTask}
+      grep "${ShellDir}/" ${ListCron} | grep -E " j[drx]_\w+" | perl -pe "s|.+ (j[drx]_\w+).*|\1|" | sort -u > ${ListTask}
     fi
-    grep -E "j[drx]_\w+\.js" ${ScriptsDir}/docker/crontab_list.sh | perl -pe "s|.+(j[drx]_\w+)\.js.+|\1|" | sort > ${ListJs}
+    cat ${ListCronLxk} | grep -E "j[drx]_\w+\.js" | perl -pe "s|.+(j[drx]_\w+)\.js.+|\1|" | sort -u > ${ListJs}
     grep -vwf ${ListTask} ${ListJs} > ${ListJsAdd}
     grep -vwf ${ListJs} ${ListTask} > ${ListJsDrop}
   else
@@ -176,8 +146,8 @@ function Notify_Version {
   if [ -f ${FileConf} ] && [[ "${VerConf}" != "${VerConfSample}" ]] && [[ ${UpdateDate} == $(date "+%Y-%m-%d") ]]
   then
     if [ ! -f ${SendCount} ]; then
-      echo -e "检测到配置文件config.sh.sample有更新\n\n更新日期: ${UpdateDate}\n当前版本: ${VerConf}\n新的版本: ${VerConfSample}\n更新内容: ${UpdateContent}\n\n如需使用新功能按该文件前几行注释操作，否则请无视本消息。\n" | tee ${ContentVersion}
-      echo -e "本消息只在该新版本配置文件更新当天发送一次，脚本地址：${ShellURL}" >> ${ContentVersion}
+      echo -e "检测到配置文件config.sh.sample有更新\n\n更新日期: ${UpdateDate}\n当前版本: ${VerConf}\n新的版本: ${VerConfSample}\n更新内容: ${UpdateContent}\n如需使用新功能请对照config.sh.sample，将相关新参数手动增加到你自己的config.sh中，否则请无视本消息。\n" | tee ${ContentVersion}
+      echo -e "本消息只在该新版本配置文件更新当天发送一次。" >> ${ContentVersion}
       cd ${ShellDir}
       node update.js
       if [ $? -eq 0 ]; then
@@ -196,7 +166,7 @@ function Npm_InstallSub {
   if [ -n "${isTermux}" ]
   then
     npm install --no-bin-links || npm install --no-bin-links --registry=https://registry.npm.taobao.org
-  elif ! type yarn
+  elif ! type yarn >/dev/null 2>&1
   then
     npm install || npm install --registry=https://registry.npm.taobao.org
   else
@@ -255,7 +225,8 @@ function Output_ListJsDrop {
 }
 
 ## 自动删除失效的脚本与定时任务，需要5个条件：1.AutoDelCron 设置为 true；2.正常更新js脚本，没有报错；3.js-drop.list不为空；4.crontab.list存在并且不为空；5.已经正常运行过npm install
-## 如果检测到某个定时任务在 scripts/docker/crontab_list.sh 中已删除，那么在本地也删除对应定时任务
+## 检测文件：LXK9301/jd_scripts 仓库中的 docker/crontab_list.sh
+## 如果检测到某个定时任务在上述检测文件中已删除，那么在本地也删除对应定时任务
 function Del_Cron {
   if [ "${AutoDelCron}" = "true" ] && [ -s ${ListJsDrop} ] && [ -s ${ListCron} ] && [ -d ${ScriptsDir}/node_modules ]; then
     echo -e "开始尝试自动删除定时任务如下：\n"
@@ -271,15 +242,16 @@ function Del_Cron {
     crontab -l
     echo -e "\n--------------------------------------------------------------\n"
     if [ -d ${ScriptsDir}/node_modules ]; then
-      echo -e "jd-base脚本成功删除失效的定时任务：\n\n${JsDrop}\n\n脚本地址：${ShellURL}" > ${ContentDropTask}
+      echo -e "删除失效的定时任务：\n\n${JsDrop}" > ${ContentDropTask}
       Notify_DropTask
     fi
   fi
 }
 
 ## 自动增加新的定时任务，需要5个条件：1.AutoAddCron 设置为 true；2.正常更新js脚本，没有报错；3.js-add.list不为空；4.crontab.list存在并且不为空；5.已经正常运行过npm install
-## 如果检测到 scripts/docker/crontab_list.sh 中增加新的定时任务，那么在本地也增加
-## 本功能生效时，会自动从 scripts/docker/crontab_list.sh 文件新增加的任务中读取时间，该时间为北京时间
+## 检测文件：LXK9301/jd_scripts 仓库中的 docker/crontab_list.sh
+## 如果检测到检测文件中增加新的定时任务，那么在本地也增加
+## 本功能生效时，会自动从检测文件新增加的任务中读取时间，该时间为北京时间
 function Add_Cron {
   if [ "${AutoAddCron}" = "true" ] && [ -s ${ListJsAdd} ] && [ -s ${ListCron} ] && [ -d ${ScriptsDir}/node_modules ]; then
     echo -e "开始尝试自动添加定时任务如下：\n"
@@ -293,7 +265,7 @@ function Add_Cron {
       then
         echo "4 0,9 * * * bash ${ShellJd} ${Cron}" >> ${ListCron}
       else
-        grep -E "\/${Cron}\." "${ScriptsDir}/docker/crontab_list.sh" | perl -pe "s|(^.+)node */scripts/(j[drx]_\w+)\.js.+|\1bash ${ShellJd} \2|" >> ${ListCron}
+        cat ${ListCronLxk} | grep -E "\/${Cron}\." | perl -pe "s|(^.+)node */scripts/(j[drx]_\w+)\.js.+|\1bash ${ShellJd} \2|" >> ${ListCron}
       fi
     done
 
@@ -304,26 +276,17 @@ function Add_Cron {
       crontab -l
       echo -e "\n--------------------------------------------------------------\n"
       if [ -d ${ScriptsDir}/node_modules ]; then
-        echo -e "jd-base脚本成功添加新的定时任务：\n\n${JsAdd}\n\n脚本地址：${ShellURL}" > ${ContentNewTask}
+        echo -e "成功添加新的定时任务：\n\n${JsAdd}" > ${ContentNewTask}
         Notify_NewTask
       fi
     else
       echo -e "添加新的定时任务出错，请手动添加...\n"
       if [ -d ${ScriptsDir}/node_modules ]; then
-        echo -e "jd-base脚本尝试自动添加以下新的定时任务出错，请手动添加：\n\n${JsAdd}" > ${ContentNewTask}
+        echo -e "尝试自动添加以下新的定时任务出错，请手动添加：\n\n${JsAdd}" > ${ContentNewTask}
         Notify_NewTask
       fi
     fi
   fi
-}
-
-## 每天定时任务中git_pull.sh只执行一次，每次运行git_pull.sh时随机生成下一天执行git_pull.sh的任务时间，生成的时间范围：7:00-11:59
-## 不影响手动执行，手动执行会刷新下一天git_pull.sh的执行时间
-function Update_Cron {
-  RanSec=$((${RANDOM} % 60))
-  RanHour=$((${RANDOM} % 5 + 7))
-  perl -i -pe "{s|18 10,14(.+jd_joy_run.*)|18 11,14\1|; s|10 10,11(.+jd_joy_run.*)|18 11,14\1|; s|.+(bash.+git_pull.*)|${RanSec} ${RanHour} * * * \1|}" ${ListCron}
-  crontab ${ListCron}
 }
 
 ## 在日志中记录时间与路径
@@ -335,37 +298,18 @@ if [ "${TZ}" = "UTC" ]; then
   echo -n "北京时间："
   echo $(date -d "8 hour" "+%Y-%m-%d %H:%M:%S")
 fi
-echo -e "\nSHELL脚本目录：${ShellDir}\n"
-echo -e "JS脚本目录：${ScriptsDir}\n"
+echo -e "\nJS脚本目录：${ScriptsDir}\n"
 echo -e "--------------------------------------------------------------\n"
 
-## 更新shell脚本、检测配置文件版本并将sample/config.sh.sample复制到config目录下
-Git_PullShell && Update_Cron
-VerConfSample=$(grep " Version: " ${FileConfSample} | perl -pe "s|.+v((\d+\.?){3})|\1|")
-[ -f ${FileConf} ] && VerConf=$(grep " Version: " ${FileConf} | perl -pe "s|.+v((\d+\.?){3})|\1|")
-if [ ${ExitStatusShell} -eq 0 ]
-then
-  echo -e "\nshell脚本更新完成...\n"
-  if [ -n "${isDocker}" ] && [ -d ${ConfigDir} ]; then
-    cp -f ${FileConfSample} ${ConfigDir}/config.sh.sample
-  fi
-else
-  echo -e "\nshell脚本更新失败，请检查原因后再次运行git_pull.sh，或等待定时任务自动再次运行git_pull.sh...\n"
-fi
+## 更新crontab
+[[ $(date "+%-H") -le 2 ]] && Update_Cron
 
 ## 克隆或更新js脚本
-if [ ${ExitStatusShell} -eq 0 ]; then
-  echo -e "--------------------------------------------------------------\n"
-  [ -f ${ScriptsDir}/package.json ] && PackageListOld=$(cat ${ScriptsDir}/package.json)
-  if [ -d ${ScriptsDir}/.git ]; then
-    Git_PullScripts
-  else
-    Git_CloneScripts
-  fi
-fi
+[ -f ${ScriptsDir}/package.json ] && PackageListOld=$(cat ${ScriptsDir}/package.json)
+[ -d ${ScriptsDir}/.git ] && Git_PullScripts || Git_CloneScripts
 
 ## 执行各函数
-if [ ${ExitStatusScripts} -eq 0 ]
+if [[ ${ExitStatusScripts} -eq 0 ]]
 then
   echo -e "js脚本更新完成...\n"
   Change_ALL
@@ -382,7 +326,7 @@ else
 fi
 
 ## 调用用户自定义的diy.sh
-if [ "${EnableExtraShell}" = "true" ]; then
+if [[ ${EnableExtraShell} == true ]]; then
   if [ -f ${FileDiy} ]
   then
     . ${FileDiy}
