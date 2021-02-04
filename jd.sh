e3ca7b694280b0c265e1f2c06db942cea36dfd27
myvmiao <76433043+myvmiao@users.noreply.github.com>
2021-02-04 06:48:04 +0000

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -1,31 +1,14 @@
 #!/usr/bin/env bash
 
-## Author: Evine Deng
-## Source: https://github.com/EvineDeng/jd-base
-## Modified： 2021-01-12
-## Version： v3.6.9
-
 ## 路径
-if [ -f /proc/1/cgroup ]
-then
-  isDocker=$(cat /proc/1/cgroup | grep docker)
-else
-  isDocker=""
-fi
-
-if [ -z "${isDocker}" ]
-then
-  ShellDir=$(cd $(dirname $0); pwd)
-else
-  ShellDir=${JD_DIR}
-fi
-
+ShellDir=${JD_DIR:-$(cd $(dirname $0); pwd)}
+[ ${JD_DIR} ] && HelpJd=jd || HelpJd=jd.sh
 ScriptsDir=${ShellDir}/scripts
 ConfigDir=${ShellDir}/config
 FileConf=${ConfigDir}/config.sh
 FileConfSample=${ShellDir}/sample/config.sh.sample
 LogDir=${ShellDir}/log
-ListScripts=$(ls ${ScriptsDir} | grep -E "j[drx]_\w+\.js" | perl -pe "s|\.js||")
+ListScripts=($(cd ${ScriptsDir}; ls *.js | grep -E "j[drx]_"))
 ListCron=${ConfigDir}/crontab.list
 
 ## 导入config.sh
@@ -52,58 +35,25 @@ function Detect_Cron {
 
 ## 用户数量UserSum
 function Count_UserSum {
-  i=1
-  while [ ${i} -le 1000 ]
-  do
-    TmpCK=Cookie${i}
-    eval CookieTmp=$(echo \$${TmpCK})
-    if [ -n "${CookieTmp}" ]
-    then
-      UserSum=${i}
-    else
-      break
-    fi
-    let i++
+  for ((i=1; i<=1000; i++)); do
+    Tmp=Cookie$i
+    CookieTmp=${!Tmp}
+    [[ ${CookieTmp} ]] && UserSum=$i || break
   done
 }
 
 ## 组合Cookie和互助码子程序
 function Combin_Sub {
   CombinAll=""
-  i=1
-  while [ $i -le ${UserSum} ]
-  do
+  for ((i=1; i<=${UserSum}; i++)); do
+    for num in ${TempBlockCookie}; do
+      if [[ $i -eq $num ]]; then
+        continue 2
+      fi
+    done
     Tmp1=$1$i
-    eval Tmp2=$(echo \$${Tmp1})
-    case $# in
-      1)
-        CombinAll="${CombinAll}&${Tmp2}"
-        ;;
-      2)
-        CombinAll="${CombinAll}&${Tmp2}@$2"
-        ;;
-      3)
-        if [ $(($i % 2)) -eq 1 ]; then
-          CombinAll="${CombinAll}&${Tmp2}@$2"
-        else
-          CombinAll="${CombinAll}&${Tmp2}@$3"
-        fi
-        ;;
-      4)
-        case $(($i % 3)) in
-          1)
-            CombinAll="${CombinAll}&${Tmp2}@$2"
-            ;;
-          2)
-            CombinAll="${CombinAll}&${Tmp2}@$3"
-            ;;
-          0)
-            CombinAll="${CombinAll}&${Tmp2}@$4"
-            ;;
-        esac
-        ;;
-    esac
-    let i++
+    Tmp2=${!Tmp1}
+    CombinAll="${CombinAll}&${Tmp2}"
   done
   echo ${CombinAll} | perl -pe "{s|^&||; s|^@+||; s|&@|&|g; s|@+|@|g}"
 }
@@ -111,17 +61,24 @@ function Combin_Sub {
 ## 组合Cookie、Token与互助码
 function Combin_All {
   export JD_COOKIE=$(Combin_Sub Cookie)
-  export FRUITSHARECODES=$(Combin_Sub ForOtherFruit "e6e04602d5e343258873af1651b603ec@52801b06ce2a462f95e1d59d7e856ef4@5bc73a365ff74a559bdee785ea97fcc5" "6d402dcfae1043fba7b519e0d6579a6f@5efc7fdbb8e0436f8694c4c393359576@6dc9461f662d490991a31b798f624128" "e2fd1311229146cc9507528d0b054da8@30f29addd75d44e88fb452bbfe9f2110@1d02fc9e0e574b4fa928e84cb1c5e70b")
+  export FRUITSHARECODES=$(Combin_Sub ForOtherFruit)
   export PETSHARECODES=$(Combin_Sub ForOtherPet)
-  export PLANT_BEAN_SHARECODES=$(Combin_Sub ForOtherBean "mze7pstbax4l7u5ggn5y2olhfy@3nwlq2wyvmz7sn4d5akh4rnrczsih2dehcx7as4ym6fgb3q7y5tq@kjno6k3dvsn4jrzs4yspokax3ud5cqqedkdv6bi@olmijoxgmjutzoaamsfbxewhiix3znzagvxr6ia" "mze7pstbax4l7dmo4vq6wz7vgu@rsuben7ys7sfbu5eub7knbibke@olmijoxgmjutzexyge246xwmaxy43t3jsqc74zy" "olmijoxgmjutybihibx67mwivxbag4rjviz3cji@m6mhupvfogvf5kuwe3c5h5fptd2syad6cznse4i@4npkonnsy7xi3mi4ngwtraxgzwabeyj7oky5rly")
-  export DREAM_FACTORY_SHARE_CODES=$(Combin_Sub ForOtherDreamFactory "xYd7cjQ3c1LyUse79rEFnw==@6E5_eFU3YHRLTljqYh_B1fg9iKwFvbWQsugw1xHcY3Q=@540bj4eZfgx_G4gtfA4_9A==@5AnP-NWntIbO2rEf58NCnA==" "CNt5BX1eD8Tw-Wq045YSWg==@phEELHGm3o7VKPIyiBO3Vw==@z-tDlNURI5HvM4MtehtjDA==@dzM8y-1G-D1pt6If32xQ0A==" "XCO7kpq00mMmYwOag2O_CQ==@48wAKDXkEE-RNwNs7W48MlW77AibIyB8QyD22ydJ4NI=@fzeFwj_aACkm-VgdmLqOhw==")
+  export PLANT_BEAN_SHARECODES=$(Combin_Sub ForOtherBean)
+  export DREAM_FACTORY_SHARE_CODES=$(Combin_Sub ForOtherDreamFactory)
   export DDFACTORY_SHARECODES=$(Combin_Sub ForOtherJdFactory)
   export JDZZ_SHARECODES=$(Combin_Sub ForOtherJdzz)
   export JDJOY_SHARECODES=$(Combin_Sub ForOtherJoy)
-  export JXNC_SHARECODES=$(Combin_Sub ForOtherJxnc "e8dd4ed6a87055a2b37982066d8910da@7d645f46ad80cddf7d6d91b4fc39f572")
+  export JXNC_SHARECODES=$(Combin_Sub ForOtherJxnc)
   export JXNCTOKENS=$(Combin_Sub TokenJxnc)
-  export BOOKSHOP_SHARECODES=$(Combin_Sub ForOtherBookShop "aea9a9e0bc9e4f49b0515020e7bbaa90@4e012467d3da47268df4ef821a9f0662@8c3cefd0dcbb4b83a32f4dffde72fa26")
-  export JD_CASH_SHARECODES=$(Combin_Sub ForOtherCash "Vl1uMrk@9qqduGQCv26BJ-NiHfexAcc_08V6HjOh@eU9Yauq2M6918jzSw3oX0w@IRwwaei6bvkgnjM" "Vl1uMrmyZvs@eU9YarrjM_53p27dyXQa3g@9Jq0uXglsVCqKd5kEv-D@9YmhuUccv2W6J9VsHue5AQqJ" "eU9YarjhYqonpDrTzXcR1Q@eU9Ya77gZK5z-TqHn3UWhQ@eU9Yaui2ZP4gpG-Gz3EThA@eU9YaeizbvQnpG_SznIS0w")
+  export BOOKSHOP_SHARECODES=$(Combin_Sub ForOtherBookShop)
+  export JD_CASH_SHARECODES=$(Combin_Sub ForOtherCash)
+  export JDNIAN_SHARECODES=$(Combin_Sub ForOtherNian)
+  export JDNIANPK_SHARECODES=$(Combin_Sub ForOtherNianPk)
+  export JDSXSY_SHARECODES=$(Combin_Sub ForOtherImmortal)
+  export JDSGMH_SHARECODES=$(Combin_Sub ForOtherSgmh)
+  export JSMOBILEFESTIVAL_SHARECODES=$(Combin_Sub ForOtherJdMobileFestival)
+  export JD818_SHARECODES=$(Combin_Sub ForOtherJd818)
+  export JDNY_SHARECODES=$(Combin_Sub ForOtherNY)
 }
 
 ## 转换JD_BEAN_SIGN_STOP_NOTIFY或JD_BEAN_SIGN_NOTIFY_SIMPLE
@@ -149,75 +106,71 @@ function Set_Env {
   Trans_UN_SUBSCRIBES
 }
 
-## 随机延迟子程序
-function Random_DelaySub {
-  CurDelay=$((${RANDOM} % ${RandomDelay} + 1))
-  echo -e "\n命令未添加 \"now\"，随机延迟 ${CurDelay} 秒后再执行任务，如需立即终止，请按 CTRL+C...\n"
-  sleep ${CurDelay}
-}
-
-## 随机延迟判断
+## 随机延迟
 function Random_Delay {
-  if [ -n "${RandomDelay}" ] && [ ${RandomDelay} -gt 0 ]; then
-    CurMin=$(date "+%M")
-    if [ ${CurMin} -gt 2 ] && [ ${CurMin} -lt 30 ]; then
-      Random_DelaySub
-    elif [ ${CurMin} -gt 31 ] && [ ${CurMin} -lt 59 ]; then
-      Random_DelaySub
+  if [[ -n ${RandomDelay} ]] && [[ ${RandomDelay} -gt 0 ]]; then
+    CurMin=$(date "+%-M")
+    if [[ ${CurMin} -gt 2 && ${CurMin} -lt 30 ]] || [[ ${CurMin} -gt 31 && ${CurMin} -lt 59 ]]; then
+      CurDelay=$((${RANDOM} % ${RandomDelay} + 1))
+      echo -e "\n命令未添加 \"now\"，随机延迟 ${CurDelay} 秒后再执行任务，如需立即终止，请按 CTRL+C...\n"
+      sleep ${CurDelay}
     fi
   fi
 }
 
 ## 使用说明
 function Help {
-  echo -e "本脚本的用法为：\n"
-  if [ -n "${isDocker}" ]
-  then
-    echo -e "1. bash jd xxx      # 如果设置了随机延迟并且当时时间不在0-2、30-31、59分内，将随机延迟一定秒数\n"
-    echo -e "2. bash jd xxx now  # 无论是否设置了随机延迟，均立即运行\n"
-    echo -e "3. bash jd hangup   # 重启挂机程序\n"
-    echo -e "4. bash jd resetpwd # 重置控制面板用户名和密码\n"
-  else
-    echo -e "1. bash jd.sh xxx      # 如果设置了随机延迟并且当时时间不在0-2、30-31、59分内，将随机延迟一定秒数\n"
-    echo -e "2. bash jd.sh xxx now  # 无论是否设置了随机延迟，均立即运行\n"
-    echo -e "3. bash jd.sh hangup   # 重启挂机程序\n"
-    echo -e "4. bash jd.sh resetpwd # 重置控制面板用户名和密码\n"
-  fi
-  echo -e "针对用法1、用法2中的\"xxx\"，无需输入后缀\".js\"，另外，如果前缀是\"jd_\"的话前缀也可以省略...\n"
-  echo -e "当前有以下脚本可以运行（包括尚未被lxk0301大佬放进docker下crontab的脚本，但不含自定义脚本）：\n"
-  echo -e "${ListScripts}\n"
+  echo -e "本脚本的用法为："
+  echo -e "1. bash ${HelpJd} xxx      # 如果设置了随机延迟并且当时时间不在0-2、30-31、59分内，将随机延迟一定秒数"
+  echo -e "2. bash ${HelpJd} xxx now  # 无论是否设置了随机延迟，均立即运行"
+  echo -e "3. bash ${HelpJd} hangup   # 重启挂机程序"
+  echo -e "4. bash ${HelpJd} resetpwd # 重置控制面板用户名和密码"
+  echo -e "\n针对用法1、用法2中的\"xxx\"，无需输入后缀\".js\"，另外，如果前缀是\"jd_\"的话前缀也可以省略。"
+  echo -e "当前有以下脚本可以运行（仅列出以jd_、jr_、jx_开头的脚本）："
+  cd ${ScriptsDir}
+  for ((i=0; i<${#ListScripts[*]}; i++)); do
+    Name=$(grep "new Env" ${ListScripts[i]} | awk -F "'|\"" '{print $2}')
+    echo -e "$(($i + 1)).${Name}：${ListScripts[i]}"
+  done
 }
 
 ## nohup
 function Run_Nohup {
-  nohup node ${js}.js > ${LogFile} &
-}
-
-## 运行挂机脚本
-function Run_HangUp {
-  Import_Conf && Detect_Cron && Set_Env
-  HangUpJs="jd_crazy_joy_coin"
-  
   for js in ${HangUpJs}
   do
     if [[ $(ps -ef | grep "${js}" | grep -v "grep") != "" ]]; then
-      if [ -n "${isDocker}" ]
-      then
-        ps -ef | grep "${js}" | grep -v "grep" | awk '{print $1}' | xargs kill -9
-      else
-        ps -ef | grep "${js}" | grep -v "grep" | awk '{print $2}' | xargs kill -9
-      fi
+      ps -ef | grep "${js}" | grep -v "grep" | awk '{print $2}' | xargs kill -9
     fi
   done
 
   for js in ${HangUpJs}
   do
-    cd ${ScriptsDir}
     [ ! -d ${LogDir}/${js} ] && mkdir -p ${LogDir}/${js}
     LogTime=$(date "+%Y-%m-%d-%H-%M-%S")
     LogFile="${LogDir}/${js}/${LogTime}.log"
-    Run_Nohup >/dev/null 2>&1
+    nohup node ${js}.js > ${LogFile} &
   done
+}
+
+## pm2
+function Run_Pm2 {
+  pm2 flush
+  for js in ${HangUpJs}
+  do
+    pm2 restart ${js}.js || pm2 start ${js}.js
+  done
+}
+
+## 运行挂机脚本
+function Run_HangUp {
+  Import_Conf $1 && Detect_Cron && Set_Env
+  HangUpJs="jd_crazy_joy_coin"
+  cd ${ScriptsDir}
+  if type pm2 >/dev/null 2>&1; then
+    Run_Pm2 2>/dev/null
+  else
+    Run_Nohup >/dev/null 2>&1
+  fi
 }
 
 ## 重置密码
@@ -228,7 +181,7 @@ function Reset_Pwd {
 
 ## 运行京东脚本
 function Run_Normal {
-  Import_Conf && Detect_Cron && Set_Env
+  Import_Conf $1 && Detect_Cron && Set_Env
   
   FileNameTmp1=$(echo $1 | perl -pe "s|\.js||")
   FileNameTmp2=$(echo $1 | perl -pe "{s|jd_||; s|\.js||; s|^|jd_|}")
